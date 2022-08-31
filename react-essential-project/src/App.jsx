234fb099dcb6f0cf0c72bb14f318ae15fc94d2bf
alekspanteli <alekspanteli@rambler.ru>
2022-08-31 12:58:12 +0000

    Update App.jsx and main.jsx

--- react-essential-project/src/App.jsx
+++ react-essential-project/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
-const [firstCity] = ["Ayia Napa", "Limassol", "Larnaca"];
-
-console.log(firstCity);
-
-const App = ({ library }) => {
-  return <>Hello from {library}</>;
+const App = () => {
+  const [emotion, setEmotion] = useState("happy");
+  return (
+    <>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>excited</button>
+    </>
+  );
 };
 
 export default App;
--- react-essential-project/src/main.jsx
+++ react-essential-project/src/main.jsx
@@ -5,6 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App library="React" />
+    <App />
   </React.StrictMode>
 );
