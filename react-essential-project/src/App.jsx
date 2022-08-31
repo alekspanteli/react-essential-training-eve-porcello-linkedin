9d382d57dc9dbac1176b58b46743c176e1139dd1
alekspanteli <alekspanteli@rambler.ru>
2022-08-31 13:49:44 +0000

    Update 2 files

--- react-essential-project/src/App.jsx
+++ react-essential-project/src/App.jsx
@@ -1,17 +1,26 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useRef } from "react";
 
 const App = () => {
-  const [checked, setChecked] = useReducer((checked) => !checked, false);
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
 
   return (
     <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>{checked ? "checked" : "not checked"}</label>
+      <form onSubmit={submit}>
+        <input ref={txtTitle} type="text" placeholder="color title..." />
+        <input ref={hexColor} type="color" />
+        <button>Add</button>
+      </form>
     </>
   );
 };
