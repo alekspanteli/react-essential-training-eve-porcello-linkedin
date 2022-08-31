8f7a611257be885824a6a55b2e0406ab85957171
alekspanteli <alekspanteli@rambler.ru>
2022-08-31 09:17:16 +0000

    Update 5 files

--- react-essential-project/src/App.jsx
+++ react-essential-project/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <header>
+      <p>Header goes here</p>
+    </header>
+  );
+};
 
-export default App
+const Main = () => {
+  return (
+    <main>
+      <p>Main goes here</p>
+    </main>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
