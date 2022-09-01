dd4401c0246fe60c8eb2df172b99e5b6c47e988b
alekspanteli <alekspanteli@rambler.ru>
2022-09-01 13:57:12 +0000

    Update App.jsx

--- react-essential-project/src/App.jsx
+++ react-essential-project/src/App.jsx
@@ -1,62 +1,37 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 
-const query = `
-query{
-  allLifts{
-    name
-    elevationGain
-    status
-  }
-}
-`;
+const items = [
+  { name: "Everest 1", elevation: 20000 },
+  { name: "Everest 2", elevation: 60000 },
+  { name: "Everest 3", elevation: 80000 },
+];
 
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query }),
+const List = ({ data, renderItem, renderEmpty }) => {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
 };
 
-function Lift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
-  );
-}
-
-function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://snowtooth.moonhighway.com/`, opts)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error)}</pre>;
-  if (!data) return null;
-
+const App = () => {
   return (
     <>
-      {data.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
-      ))}
+      <List
+        data={items}
+        renderEmpty={<p>This list is empty</p>}
+        renderItem={(item) => (
+          <>
+            {item.name} - {item.elevation} ft.
+          </>
+        )}
+      />
     </>
   );
-}
+};
 
 export default App;
