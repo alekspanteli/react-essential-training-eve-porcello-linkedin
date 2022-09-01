73a1113421c15ebbcb8b7821bf9867f07c8e84db
alekspanteli <alekspanteli@rambler.ru>
2022-09-01 13:17:45 +0000

    Update App.jsx

--- react-essential-project/src/App.jsx
+++ react-essential-project/src/App.jsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser({ name, location, avatar }) {
+const query = `
+query{
+  allLifts{
+    name
+    elevationGain
+    status
+  }
+}
+`;
+
+const opts = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query }),
+};
+
+function Lift({ name, elevationGain, status }) {
   return (
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={150} alt={name} />
+      <p>
+        {elevationGain} {status}
+      </p>
     </div>
   );
 }
@@ -18,24 +35,26 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(`https://api.github.com/users/alekspanteli`)
+    fetch(`https://snowtooth.moonhighway.com/`, opts)
       .then((response) => response.json())
       .then(setData)
       .then(() => setLoading(false))
       .catch(setError);
   }, []);
 
-  if (loading) return <h1>Loadding...</h1>;
+  if (loading) return <h1>Loading...</h1>;
   if (error) return <pre>{JSON.stringify(error)}</pre>;
   if (!data) return null;
 
   return (
     <>
-      <GithubUser
-        name={data.login}
-        location={data.node_id}
-        avatar={data.avatar_url}
-      />
+      {data.data.allLifts.map((lift) => (
+        <Lift
+          name={lift.name}
+          elevationGain={lift.elevationGain}
+          status={lift.status}
+        />
+      ))}
     </>
   );
 }
