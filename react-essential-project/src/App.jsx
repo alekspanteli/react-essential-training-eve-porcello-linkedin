98736c6a27a84eeb11bd0d06728e9a2f01fc0470
alekspanteli <alekspanteli@rambler.ru>
2022-09-02 09:41:57 +0000

    Update 5 files

--- react-essential-project/src/App.jsx
+++ react-essential-project/src/App.jsx
@@ -1,37 +1,58 @@
 import "./App.css";
+import { Link, Outlet } from "react-router-dom";
 
-const items = [
-  { name: "Everest 1", elevation: 20000 },
-  { name: "Everest 2", elevation: 60000 },
-  { name: "Everest 3", elevation: 80000 },
-];
-
-const List = ({ data, renderItem, renderEmpty }) => {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>{renderItem(item)}</li>
-      ))}
-    </ul>
-  );
-};
-
-const App = () => {
+const Nav = () => {
   return (
     <>
-      <List
-        data={items}
-        renderEmpty={<p>This list is empty</p>}
-        renderItem={(item) => (
-          <>
-            {item.name} - {item.elevation} ft.
-          </>
-        )}
-      />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Outlet />
     </>
   );
 };
 
-export default App;
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export const History = () => {
+  return (
+    <>
+      <h1>History</h1>
+    </>
+  );
+};
+
+export const About = () => {
+  return (
+    <>
+      <Nav />
+      <h1>About</h1>
+    </>
+  );
+};
+
+export const Contact = () => {
+  return (
+    <>
+      <Nav />
+      <h1>Contact</h1>
+    </>
+  );
+};
+
+export const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
